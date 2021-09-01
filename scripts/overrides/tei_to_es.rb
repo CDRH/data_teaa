9a7b118dca49983935cd814b97f0a3b35231fbbb
Karin Dalziel <kdalziel2@unl.edu>
2021-09-01 14:06:10 -0500

    added Theme | Empires | Ethnic Group | Religion to index"

--- scripts/overrides/tei_to_es.rb
+++ scripts/overrides/tei_to_es.rb
@@ -8,10 +8,14 @@ class TeiToEs
     xpaths = {}
     xpaths["category"] = "/TEI/teiHeader/profileDesc/textClass/keywords[@n='type'][1]/term"
     xpaths["subcategory"] = "/TEI/teiHeader/profileDesc/textClass/keywords[@n='subtype']/term"
-    xpaths["date"] = "//div2[@type='bibliography']/bibl/date/@when"
-    xpaths["date_display"] = "//div2[@type='bibliography']/bibl/date"
     xpaths["language"] = "//langUsage/language/@ident"
     xpaths["publisher"] = "//div2[@type='bibliography']/bibl/publisher"
+
+    xpaths["keywords"] = "/TEI/teiHeader/profileDesc/textClass/keywords[@n='empires'][1]/term"
+    xpaths["topics"] = "/TEI/teiHeader/profileDesc/textClass/keywords[@n='theme'][1]/term"
+
+    xpaths["ethnicgroup"] = "/TEI/teiHeader/profileDesc/textClass/keywords[@n='ethnic_group'][1]/term"
+    xpaths["religion"] = "/TEI/teiHeader/profileDesc/textClass/keywords[@n='religion'][1]/term"
 
     return xpaths
   end
@@ -34,6 +38,9 @@ class TeiToEs
   #    Field Builders    #
   ########################
 
-  
+  def assemble_collection_specific
+    @json["ethnicgroup_k"] = get_list(@xpaths["ethnicgroup"])
+    @json["religion_k"] = get_list(@xpaths["religion"])
+  end
 
 end
